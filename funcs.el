4f4c23a0b914bdd32b1d2cfde00979be3a957364
dschoenig <daniel.schoenig@posteo.de>
2019-07-19 12:21:30 -0400

    Simple focus mode and txt md switcher

--- funcs.el
+++ funcs.el
@@ -20,26 +20,6 @@
       (put 'distraction-free-toggle 'state t)
       (distraction-free-enable))))
 
-(defun distraction-free-typewriter ()
-  (interactive)
-  (if (get 'distraction-free-typewriter 'state)
-      (progn
-        (put 'distraction-free-typewriter 'state nil)
-        (setq maximum-scroll-margin maximum-scroll-margin-saved
-              scroll-margin scroll-margin-saved
-              scroll-preserve-screen-position scroll-preserve-screen-position
-              scroll-conservatively scroll-conservatively-saved))
-    (progn
-      (put 'distraction-free-typewriter 'state t)
-      (setq maximum-scroll-margin-saved maximum-scroll-margin
-            scroll-margin-saved scroll-margin
-            scroll-preserve-screen-position-saved scroll-preserve-screen-position
-            scroll-conservatively-saved scroll-conservatively)
-      (setq maximum-scroll-margin 0.5
-            scroll-margin 99999
-            scroll-preserve-screen-position t
-            scroll-conservatively 0))))
-
 (defun distraction-free-enable ()
   (interactive)
   (setq buffer-face-mode-face '(:family "iA Writer Mono V" :height 140))
@@ -69,5 +49,63 @@
   (spacemacs/toggle-highlight-current-line-globally-on)
   (spacemacs/toggle-vi-tilde-fringe-on)
   )
+
+(defun distraction-free-typewriter ()
+  (interactive)
+  (if (get 'distraction-free-typewriter 'state)
+      (progn
+        (put 'distraction-free-typewriter 'state nil)
+        (setq maximum-scroll-margin maximum-scroll-margin-saved
+              scroll-margin scroll-margin-saved
+              scroll-preserve-screen-position scroll-preserve-screen-position
+              scroll-conservatively scroll-conservatively-saved))
+    (progn
+      (put 'distraction-free-typewriter 'state t)
+      (setq maximum-scroll-margin-saved maximum-scroll-margin
+            scroll-margin-saved scroll-margin
+            scroll-preserve-screen-position-saved scroll-preserve-screen-position
+            scroll-conservatively-saved scroll-conservatively)
+      (setq maximum-scroll-margin 0.5
+            scroll-margin 99999
+            scroll-preserve-screen-position t
+            scroll-conservatively 0))))
+
+(defun switch-to-text-mode ()
+  (interactive)
+  (if (get 'distraction-free-toggle 'state)
+      (progn
+        (distraction-free-disable)
+        (text-mode)
+        (distraction-free-enable)
+        (spacemacs/toggle-fullscreen-frame-on)
+        )
+    (progn
+      (text-mode))))
+
+(defun switch-to-markdown-mode ()
+  (interactive)
+  (if (get 'distraction-free-toggle 'state)
+      (progn
+        (distraction-free-disable)
+        (markdown-mode)
+        (distraction-free-enable)
+        (spacemacs/toggle-fullscreen-frame-on)
+        )
+    (progn
+      (markdown-mode))))
+
+(defun distraction-free-focus ()
+  (interactive)
+  (if (get 'distraction-free-focus 'state)
+      (progn
+        (put 'distraction-free-focus 'state nil)
+        (switch-to-markdown-mode)
+        (spacemacs/toggle-highlight-current-line-globally-off)
+        (distraction-free-focus-off))
+    (progn
+      (put 'distraction-free-focus 'state t)
+      (switch-to-text-mode)
+      (spacemacs/toggle-highlight-current-line-globally-on)
+      (distraction-free-focus-on))))
 
 ;;; funcs.el ends here
--- keybindings.el
+++ keybindings.el
@@ -1,6 +1,10 @@
 (spacemacs/declare-prefix "d" "distraction free")
+(spacemacs/declare-prefix "dm" "switch mode")
 (spacemacs/set-leader-keys
   "dd" 'distraction-free-toggle
+  "df" 'distraction-free-focus
   "dt" 'distraction-free-typewriter
-  "dw" 'writegood-mode)
+  "dw" 'writegood-mode
+  "dmt" 'switch-to-text-mode
+  "dmm" 'switch-to-markdown-mode)
 
